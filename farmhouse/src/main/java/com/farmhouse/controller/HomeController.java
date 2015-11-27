19e1a1c58fed49e3e7bba875314b26e1e5f4d907
mastersunny <sunnycse248@gmail.com>
2015-11-28 01:15:51 +0600

    security added

--- farmhouse/src/main/java/com/farmhouse/controller/HomeController.java
+++ farmhouse/src/main/java/com/farmhouse/controller/HomeController.java
@@ -1,16 +1,13 @@
 package com.farmhouse.controller;
 
-import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.farmhouse.beans.CategoryBean;
-import com.farmhouse.beans.NavigationBean;
+import com.farmhouse.dao.SubCategoryDao;
 import com.farmhouse.entity.Category;
 import com.farmhouse.entity.SubCategory;
 import com.farmhouse.service.CategoryService;
@@ -23,19 +20,21 @@ public class HomeController {
 		
 	@Autowired
 	CategoryService CategoryService;
+	
+	@Autowired
+	SubCategoryDao subCategoryDao;
 
 	@RequestMapping("/")
 	public String home(Model model) {
 
-		List<Category> categories = CategoryService.getCategoryList();
-
+		List<Category> categories = null;
+		List<SubCategory> subcategories = null;
 		
+		categories = CategoryService.getCategoryList();
+	    subcategories= subCategoryDao.findAll();
 
-		if (categories != null) {
-
-			
-
-		}
+		model.addAttribute("categories", categories);
+		model.addAttribute("subcategories", subcategories);
 		
 		
 		return "home";
